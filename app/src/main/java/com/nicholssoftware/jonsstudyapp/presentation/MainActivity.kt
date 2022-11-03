6388d2b7cb431a643840b194689a0dec4fdf3469
Jon Nichols <nichojo89@gmail.com>
2022-11-02 22:44:55 -0400

    Added Bottom Nav

--- app/src/main/java/com/nicholssoftware/jonsstudyapp/presentation/MainActivity.kt
+++ app/src/main/java/com/nicholssoftware/jonsstudyapp/presentation/MainActivity.kt
@@ -2,12 +2,25 @@ package com.nicholssoftware.jonsstudyapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.nicholssoftware.jonsstudyapp.R
 import com.nicholssoftware.jonsstudyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding get() = _binding
+    private lateinit var _binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(view.root)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        _binding.bottomNav.setupWithNavController(navController)
+
+        setContentView(binding.root)
     }
 }
