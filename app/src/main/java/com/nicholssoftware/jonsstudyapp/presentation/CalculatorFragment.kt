bb413ff9e43c0277a599317359972fc1b594449e
Jon Nichols <nichojo89@gmail.com>
2022-11-06 00:50:39 -0500

    Moved calc frag

--- app/src/main/java/com/nicholssoftware/jonsstudyapp/presentation/CalculatorFragment.kt
+++ app/src/main/java/com/nicholssoftware/jonsstudyapp/presentation/CalculatorFragment.kt
@@ -1,10 +1,11 @@
-package com.nicholssoftware.jonsstudyapp
+package com.nicholssoftware.jonsstudyapp.presentation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nicholssoftware.jonsstudyapp.R
 
 
 class CalculatorFragment : Fragment() {
