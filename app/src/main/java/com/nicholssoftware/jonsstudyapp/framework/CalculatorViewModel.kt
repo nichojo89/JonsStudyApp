293da325189b645fd22d0b8e4aa1dc2d24e79532
Jon Nichols <nichojo89@gmail.com>
2022-11-06 01:59:23 -0500

    WIP

--- app/src/main/java/com/nicholssoftware/jonsstudyapp/framework/CalculatorViewModel.kt
+++ app/src/main/java/com/nicholssoftware/jonsstudyapp/framework/CalculatorViewModel.kt
@@ -1,7 +1,13 @@
 package com.nicholssoftware.jonsstudyapp.framework
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
 
 class CalculatorViewModel(application: Application): AndroidViewModel(application) {
+
+    fun addDigit(){
+        val g = 0
+        throw java.lang.Exception("test")
+    }
 }
--- app/src/main/java/com/nicholssoftware/jonsstudyapp/presentation/CalculatorFragment.kt
+++ app/src/main/java/com/nicholssoftware/jonsstudyapp/presentation/CalculatorFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.MutableLiveData
 import com.nicholssoftware.jonsstudyapp.R
+import com.nicholssoftware.jonsstudyapp.databinding.FragmentCalculatorBinding
+import com.nicholssoftware.jonsstudyapp.framework.FragmentX
 
 
-class CalculatorFragment : Fragment() {
-    //TODO add viewmodel
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+class CalculatorFragment : FragmentX<FragmentCalculatorBinding>(R.layout.fragment_calculator) {
+
+    //Example of init logic
+    override fun FragmentCalculatorBinding.initialize() {}
+
+    //@Bindable
+    val answer = MutableLiveData<String>()
+
+    fun addDigit(){
+        val g = 0
+        throw java.lang.Exception("test")
     }
-
 }
